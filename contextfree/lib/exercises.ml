35ce1be9ca71b04606215c1076e63551de8be22e
DarkAngel8000 <manu.lepori.7@gmail.com>
2024-10-23 14:48:19 +0200

    Aggiorno esercizi

--- contextfree/lib/exercises.ml
+++ contextfree/lib/exercises.ml
@@ -7,24 +7,62 @@ let todo : grammar =
     terminals = [ '0'; '1' ];
     productions =
       [
-        S --> "0S0";
-        S --> "1S1";
-        S --> "";
+        S --> "0S0"; (* 0 *)
+        S --> "1S1"; (* 1 *)
+        S --> ""; (* 2 *)
       ];
     start = S;
   }
 
 
 (* #### Exercise 1, easy (zero_n_one_n) *)
-let zero_n_one_n : grammar = todo
+let zero_n_one_n : grammar = 
+  {
+    symbols = [ S ];
+    terminals = [ '0'; '1' ];
+    productions =
+      [
+        S --> ""; (* 0 *)
+        S --> "0S1"; (* 1 *)
+      ];
+    start = S;
+  }
 
 
 (* #### Exercise 2, easy (palindromes) *)
-let palindromes : grammar = todo
-
+let palindromes : grammar = 
+  {
+    symbols = [ S ];
+    terminals = [ '0'; '1' ];
+    productions =
+      [
+        S --> ""; (* 0 *)
+        S --> "0S0"; (* 1 *)
+        S --> "1S1"; (* 2 *)
+        S --> "1"; (* 3 *)
+        S --> "0"; (* 4 *)
+      ];
+    start = S;
+  }
 
 (* #### Exercise 3, medium (balanced_parentheses)*)
-let balanced_parentheses : grammar = todo
+let balanced_parentheses : grammar = 
+  {
+    symbols = [ S ];
+    terminals = [ '('; ')'; '['; ']'; '{'; '}'];
+    productions =
+      [
+        S --> ""; (* 0 *)
+        S --> "(S)"; (* 1 *)
+        S --> "[S]"; (* 2 *)
+        S --> "{S}"; (* 3 *)
+        S --> "S[]S"; (* 4 *)
+        S --> "S()S"; (* 5 *)
+        S --> "S{}S"; (* 6 *)
+
+      ];
+    start = S;
+  }
 
 
 (* #### Exercise 4, hard (same_amount)
@@ -33,4 +71,21 @@ let balanced_parentheses : grammar = todo
    Hint 2: think of the language of words where the number of 0s is one greater
    than the number of 1s and viceversa, then combine them.
 *)
-let same_amount : grammar = todo
+let same_amount : grammar = 
+  {
+    symbols = [ S ];
+    terminals = [ '0'; '1';];
+    productions =
+      [
+        S --> ""; (* 0 *)
+        S --> "01S"; (* 1 *)
+        S --> "1S0"; (* 2 *)
+        S --> "0S1"; (* 3 *)
+        S --> "10S"; (* 4 *)
+        S --> "00S11"; (* 5 *)
+        S --> "11S00"; (* 6 *)
+        S --> "0011S"; (* 7 *)
+        S --> "1100S"; (* 8 *)
+      ];
+    start = S;
+  }
